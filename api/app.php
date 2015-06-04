19442c9162a021d7b7e335e0a15494787a8be4d7
Daniel Lerner <lernerd@gmail.com>
2015-06-04 09:42:37 -0300

    API conforming to swagger.json

--- api/app.php
+++ api/app.php
@@ -9,6 +9,10 @@ require_once __DIR__.'/vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
         'driver'   => 'pdo_pgsql',
@@ -17,6 +21,19 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         'password' => '',
     ),
 ));
+
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+$app->match("{url}", function($url) use ($app) { 
+	header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS");
+	header("Access-Control-Allow-Origin: *");
+	return "OK"; 
+})->assert('url', '.*')->method("OPTIONS");
 
 $app->get('/client', function () use ($app) {
 	$sql = "SELECT * FROM clients ORDER BY name";
@@ -37,6 +54,27 @@ $app->get('/client/{id}', function ($id) use ($app) {
 	header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS");
 	header("Access-Control-Allow-Origin: *");
 	return $app->json($client, 200);
+});
+
+$app->put('/client',  function (Request $request) use ($app) {
+	$name = $request->getContent();
+	if($name) {
+		$sql = "INSERT INTO clients (name) VALUES ('".$name."')";
+		$app['db']->fetchAssoc($sql);
+	}
+
+	header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS");
+	header("Access-Control-Allow-Origin: *");
+	return $app->json(null, 200);
+});
+
+$app->delete('/client/{id}', function ($id) use ($app) {
+	$sql = "DELETE FROM clients WHERE id = ?";
+	$app['db']->fetchAssoc($sql, array((int) $id));
+
+	header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT, PATCH, OPTIONS");
+	header("Access-Control-Allow-Origin: *");
+	return $app->json(null, 200);
 });
 
 $app->run();
